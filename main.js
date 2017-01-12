913414eba0609a90fb91cbd8c0be5d9d2be1343c
Michael Tambornino <michael.tambornino@gmail.com>
2017-01-12 11:16:21 -0600

    added firebase auth

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+// add firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyCT6Olqns1kus1qwC_v10PDqIXlRloBUBE",
+    authDomain: "c17-jquery-auth-4f244.firebaseapp.com",
+    databaseURL: "https://c17-jquery-auth-4f244.firebaseio.com",
+    storageBucket: "c17-jquery-auth-4f244.appspot.com",
+    messagingSenderId: "483267125880"
+  })
+
+
+
+
 $('form').submit((e) => {
    var email = $('input[type="email"]').val()
    // set to h1
